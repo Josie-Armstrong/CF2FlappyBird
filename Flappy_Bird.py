756926ffd8c80e84e90c5a2fc1df6eebfe410fd2
j-j4y <166659170+j-j4y@users.noreply.github.com>
2024-04-12 11:11:40 -0600

    changed bird sprite to our custom ones

--- Flappy_Bird.py
+++ Flappy_Bird.py
@@ -62,7 +62,7 @@ class Bird(pygame.sprite.Sprite):
         self.index = 0
         self.counter = 0
         for num in range(1,4):
-            img = pygame.image.load(f'./Game Textures/Sprites/Birds L1/bird{num}.png')
+            img = pygame.image.load(f'./Game Textures/Sprites/Birds L1/coolbird{num}.png')
             self.images.append(img)
         self.image = self.images[self.index]
         self.rect = self.image.get_rect()
