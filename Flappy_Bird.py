240d2b827eecb85fd17f78a2b678a1ce34546552
j-j4y <166659170+j-j4y@users.noreply.github.com>
2024-04-24 15:36:20 -0600

    added + roughly centered highscore

--- Flappy_Bird.py
+++ Flappy_Bird.py
@@ -49,6 +49,7 @@ bg2 = pygame.transform.scale(bg2, (3660, 620)) # scale background image
 #define font
 font = pygame.font.SysFont('Courier', 60)
 coin_font = pygame.font.SysFont('Courier',40)
+font2 = pygame.font.SysFont('Courier', 30)
 
 #define color
 white = (255,255,255)
@@ -402,6 +403,7 @@ while True:
     if shop_open == False:
         #draw score to screen
         draw_text(str(score), font, white, int(screen_width / 2), 20)
+        draw_text(f"Highscore:{highscore}", font2, white,230, 75)
     
     # draw coin count to screen
     draw_coins()
